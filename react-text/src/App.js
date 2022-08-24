bc74ff1b9aa68ca8c3eb527004bbd94d2c442b81
Pablo Rodrigo <pablorodrigo210104@hotmail.com>
2022-08-24 17:40:27 -0300

    text editor

---
+++ react-text/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TextEditor } from './components/TextEditor';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+function App() {
+  return (
+    <div className="App">
+        <TextEditor/>
+    </div>
+  );
+}
+
+export default App;
